e9019902788f90db859f684e88fc2dd293af0243
Jangilkyu <jang19.dev@gmail.com>
2022-05-17 22:47:45 +0900

    Add: bcrypt project init

---
+++ bcrypt/app.js
@@ -0,0 +1,10 @@
+// server
+const express = require('express');
+const app = express();
+
+// start server
+const port = 3000;
+
+app.listen(port, () => {
+    console.log(`error has occured ${e}`);
+});
